3bb9a367eb65c8243b3252d09ef2e66065103053
ARVINDERJEET SINGH <69034607+Arvinderjeet@users.noreply.github.com>
2022-09-27 11:09:28 +0530

    Add files via upload

--- main/install_libs.bat
+++ main/install_libs.bat
@@ -3,3 +3,8 @@ pip install tkinter
 pip install webbrowser
 pip install pyttsx3
 pip install PyPDF2
+:: pip install OS
+:: os is already present in system
+:: pip install time
+:: time is already present in system
+pip install threading
